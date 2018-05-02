79d2b459d99ba2d3189cafaba634fa3e74246db3
greg <greg@fittedcloud.com>
2018-05-02 18:45:54 -0400

    Added RI coverage and recommendation features
    Invocation to accept a command as a first argument, "cost", "recommend", or "coverage"
    Adds -l --lookback and -r --service options for "recommend" command.
    Formatted -h --help options to fit in 80-width terminal.
    Minor changes to the default output format.

--- README.md
+++ README.md
@@ -8,34 +8,68 @@ For more information about the software, see the blog post:
 
 
 Installation:
-    1. Install Python 2.7 if not already installed.
-    2. Install boto3 and botocore.  Use "sudo pip install boto3 botocore".
+    1. Install Python 2.7 and pip2.7 if not already installed.
+    2. Install boto3 and botocore.  Use "sudo pip2.7 install boto3 botocore".
 
 Quick Start:
 ```
-$ python costreporter.py -a <aws access key> -s <aws secret key> -t <start-time as, YYYY-MM-DD>,<end-time as YYYY-MM-DD>
-$ python costreporter.py -p <profile name> -t <start-time as, YYYY-MM-DD>,<end-time as YYYY-MM-DD>
+$ # display cost information
+$ python costreporter.py cost -a <aws access key> -s <aws secret key> -t <start-time as, YYYY-MM-DD>,<end-time as YYYY-MM-DD>
+$
+$ # display reserved instance recommendations
+$ python costreporter.py recommend -a <aws access key> -s <aws secret key>
+$
+$ # display reservation coverage
+$ python costreporter.py cost -a <aws access key> -s <aws secret key> -t <start-time as, YYYY-MM-DD>,<end-time as YYYY-MM-DD>
 ```
-
 For more information about options:
 ```
-costreporter.py <options>
-	Options are:
+$ python costreporter.py -h
+costreporter.py <command> [options]
 
-	--help - Display this help message
-	-p --profile <profile name> - AWS profile name (can be used instead of -a and -s options)
-	-a --accesskey <access key> - AWS access key
-	-s --secretkey <secret key> - AWS secret key
-	-r --regions <region1,region2,...> - A list of AWS regions.  If this option is omitted, all regions will be checked.
-	-t --timerange - Time range as <start,end> time in format <YYYY-MM-DD>,<YYYY-MM-DD>
-	-j --json - Output in JSON format.
-	-c --csv - Output as CSV.  Not compatible with --json.
-	-d --dimension <dimension> - Group output by dimension (examples: 'AZ','INSTANCE_TYPE','LINKED_ACCOUNT','OPERATION','PURCHASE_TYPE','REGION','SERVICE','USAGE_TYPE','USAGE_TYPE_GROUP','RECORD_TYPE','OPERATING_SYSTEM','TENANCY','SCOPE','PLATFORM','SUBSCRIPTION_ID','LEGAL_ENTITY_NAME','DEPLOYMENT_OPTION','DATABASE_ENGINE','CACHE_ENGINE','INSTANCE_TYPE_FAMILY')
-	-g --tag <tag name> - Group by tag name (list of names in format Tag1,Tag2,...,TagN).
-	-i --interval <interval> - Dumps stats at <interval> granularity.  Valid values are MONTHLY (default) and DAILY.	One of the following three parameters are required:
-		1. Both the -a and -s options.
-		2. The -p option.
-		3. A valid AWS_DEFAULT_PROFILE environment variable.
+    Command must be one of the following:
 
-	Depending on the number of EBS volumes being analyzed, this tool may take several minutes to run.
+    cost - Report cost data
+    coverage - Report reservation coverage
+    recommend - Report reserved instance recommendations
+
+    General options are:
+
+        -h --help - Display this help message
+        -p --profile <profile name> - AWS profile name
+                (can be used instead of -a and -s options)
+        -a --accesskey <access key> - AWS access key
+        -s --secretkey <secret key> - AWS secret key
+        -j --json - Output in JSON format.
+        -c --csv - Output as CSV.  Not compatible with --json
+                (currently not available for 'recommend' command).
+
+    Options for 'cost' and 'coverage' commands:
+
+        -t --timerange - Time range as <start,end> time
+                in format <YYYY-MM-DD>,<YYYY-MM-DD> (required)
+        -d --dimension <dimension> - Group output by dimension
+                (examples: AZ,INSTANCE_TYPE,LINKED_ACCOUNT,OPERATION,
+                 PURCHASE_TYPE,REGION,SERVICE (default),USAGE_TYPE,
+                 USAGE_TYPE_GROUP,RECORD_TYPE,OPERATING_SYSTEM,
+                 TENANCY,SCOPE,PLATFORM,SUBSCRIPTION_ID,LEGAL_ENTITY_NAME,
+                 DEPLOYMENT_OPTION,DATABASE_ENGINE,CACHE_ENGINE,
+                 INSTANCE_TYPE_FAMILY)
+        -g --tag <tag name> - Group by tag name
+                (list of names in format Tag1,Tag2,...,TagN).
+        -i --interval <interval> - Dumps stats at <interval> granularity.
+                Valid values are MONTHLY (default) and DAILY.
+
+    Options for 'recommend' command:
+
+        -l --lookback <lookback> - Lookback period for recommendations.
+                Valid values are SEVEN_DAYS, THIRTY_DAYS,
+                SIXTY_DAYS (default)
+        -r --service <service> - Service for recommendations.
+                Valid values are EC2 (default) and RDS
+
+    One of the following three parameters are required:
+        1. Both the -a and -s options.
+        2. The -p option.
+        3. A valid AWS_DEFAULT_PROFILE environment variable.
 ```
--- costreporter.py
+++ costreporter.py
@@ -41,6 +41,12 @@ FC_MATCH_DATE = "[0-9]{4}-[0-1][0-9]-[0-3][0-9]"
 # current valid values are MONTHLY and DAILY
 FC_INTERVALS = ["MONTHLY", "DAILY"]
 
+# supported RI recommendation services are EC2 and RDS
+FC_RI_SERVICES = ["EC2", "RDS"]
+
+# valid commands in the form of {<command name>: <command description>}
+FC_COMMANDS = {"cost": "Report cost data", "recommend": "Report reserved instance recommendations", "coverage": "Report reservation coverage"}
+
 # a list of currently available dimensions by which to group
 GROUP_DIMENSIONS = ["AZ",
                     "INSTANCE_TYPE",
@@ -102,6 +108,14 @@ ABBRV = {
     "Refund": "Ref" # everyone's favorite
 } 
 
+# simple check to see if string can be converted to float
+def isfloat(value):
+  try:
+    float(value)
+    return True
+  except ValueError:
+    return False
+
 # simple abbreviation scheme:
 #
 # strip off beginning "Amazon" and "AWS" from service name
@@ -144,6 +158,73 @@ def build_abbreviations(a, s, r, start, end):
         traceback.print_exc()
     return abbrv
 
+# "get res recs" uses lookback instead of start/end time.
+# currently, supported services are EC2 and RDS
+def get_reserve_instance_recs(a, s, service="EC2", lookback="SIXTY_DAYS"):
+
+    if service == "EC2":
+        service = "Amazon Elastic Compute Cloud - Compute"
+    elif service == "RDS":
+        service = "Amazon Relational Database Service"
+    ce = boto3.client('ce',
+                      aws_access_key_id=a,
+                      aws_secret_access_key=s,
+                      region_name="us-east-1") # not sure if region matters
+    res = ce.get_reservation_purchase_recommendation(
+        Service=service,
+        LookbackPeriodInDays=lookback,
+        TermInYears="ONE_YEAR",
+        )
+    #pprint.pprint(res, indent=1)
+    return res
+
+def get_reservation_coverage(a, s, rlist, start, end, dims, tags, granularity="MONTHLY"):
+    covs = []
+
+    groupbys = []
+    if len(dims) > 0 or len(tags) > 0:
+        dims = dims.split(",")
+        for d in dims:
+            groupbys.append({"Type":"DIMENSION", "Key":d})
+
+        tags = tags.split(",")
+    if len(tags) > 0 and tags != [""]:
+        for t in tags:
+            groupbys.append({"Type":"TAG", "Key":t})
+
+    if len(groupbys) == 0: # group by service by default
+        groupbys.append({"Type":"DIMENSION", "Key":"REGION"})
+
+    try:
+        for r in rlist:
+            ce = boto3.client('ce',
+                              aws_access_key_id=a,
+                              aws_secret_access_key=s,
+                              region_name=r)
+
+            # can either have granularity or groupby, but not both
+            if len(groupbys) > 0:
+                res = ce.get_reservation_coverage(TimePeriod={"Start":start, "End":end},
+                                                  GroupBy=groupbys)
+            else:
+                res = ce.get_reservation_coverage(TimePeriod={"Start":start, "End":end},
+                                                  Granularity=granularity)
+            cbt = res['CoveragesByTime']
+            for groups in cbt:
+                for group in groups['Groups']:
+                    c = {
+                        "start_time": groups['TimePeriod']['Start'],
+                        "end_time": groups['TimePeriod']['End'],
+                        "Attributes": group['Attributes'],
+                        "Coverage": group['Coverage']['CoverageHours']
+                    }
+                    covs.append(c)
+    except:
+        e = sys.exc_info()
+        print("ERROR: exception region=%s, error=%s" %(r, str(e)))
+        traceback.print_exc()
+    return covs
+
 def get_costs(a, s, rlist, start, end, dims, tags, granularity="MONTHLY"):
     costs = []
 
@@ -185,8 +266,8 @@ def get_costs(a, s, rlist, start, end, dims, tags, granularity="MONTHLY"):
                     cost = {
                         "region": r,
                         "estimated": groups['Estimated'],
-                        "time_start": groups['TimePeriod']['Start'],
-                        "time_end": groups['TimePeriod']['End'],
+                        "start_time": groups['TimePeriod']['Start'],
+                        "end_time": groups['TimePeriod']['End'],
                         "group": group['Keys'],
                         #"srvabbr": ABBRV[group['Keys'][0]],
                         "blended_cost": group['Metrics']['BlendedCost'],
@@ -217,7 +298,7 @@ def flatten(d, parent_key='', sep='_'):
 
 # output is [{'group': 'name', values:{'unblended_cost':xxx, 'unblended_unit':xxx,
 #             'usage_quantity':xxx, 'usage_unit':xxx}}]
-def consolidate_by_group(costs):
+def consolidate_costs_by_group(costs):
     out = []
     for cost in costs:
         found = 0
@@ -241,8 +322,88 @@ def consolidate_by_group(costs):
             out.append(tmp)
     return out
 
+# pass return value from get_reserve_instance_recs
+def print_ri_recs_results(recs, use_json=False, use_csv=False, lookback="SIXTY_DAYS", term="ONE_YEAR"):
+    if len(recs) == 0:
+        return
+
+    if use_json == True:
+        print(json.dumps(recs, sort_keys=True, indent=4))
+    elif use_csv == True:
+        #TODO the recommendation output is very nested, not ideal for CSV
+        pass
+    else:
+        print("Reserved Instance Recommendations: %s\n" %recs["Metadata"]["GenerationTimestamp"])
+        print(" = Recommendation Details =")
+        for rec in recs["Recommendations"]:
+            for detail in rec["RecommendationDetails"]:
+                for key, value in detail.items():
+                    if key == "InstanceDetails":
+                        continue # we'll print this out later
+                    if isfloat(value): # if a float
+                        print("%-54s %14.2f" %(key, float(value)))
+                    else:
+                        print("%-54s %14s" %(key, str(value)))
+
+                print("\n = Instance Details =")
+                for key, value in detail["InstanceDetails"].items():
+                    print("%s:" %key)
+                    for k, v in detail["InstanceDetails"][key].items():
+                        if isfloat(v): # if a float
+                            print("%-54s %14.2f" %(k, float(v)))
+                        else:
+                            print("%-54s %14s" %(k, str(v)))
+
+            print("\n = Recommendation Summary =")
+            for key, value in rec["RecommendationSummary"].items():
+                if isfloat(value): # if a float
+                    print("%-54s %14.2f" %(key, float(value)))
+                else:
+                    print("%-54s %14s" %(key, str(value)))
+
+# pass return value from get_reservation_coverages()
+def print_coverage_results(covs, use_json=False, use_csv=False, start=None, end=None):
+    if len(covs) == 0:
+        return
+
+    if use_json == True:
+        print(json.dumps(covs, sort_keys=True, indent=4))
+    elif use_csv == True:
+        flat_covs = []
+        for cov in covs:
+            flat_covs.append(flatten(cov))
+        # print headers
+        csv_writer = csv.DictWriter(sys.stdout, flat_covs[0].keys(), delimiter=",")
+        csv_writer.writeheader()
+        for cov in flat_covs:
+            csv_writer.writerow(cov)
+    else:
+        # for calculating totals
+        totals = {}
+
+        print("\nSummary of Reservation Coverage: %s - %s\n" %(start, end))
+        # print header.  hard-coded for now
+        for cov in covs:
+            print("= Group Attributes =")
+            for k, v in cov['Attributes'].items():
+                print("    %s: %s" %(k, v))
+            print("= Coverage =")
+            for k, v in cov['Coverage'].items():
+                if k in totals:
+                    totals[k] += float(v)
+                else:
+                    totals[k] = float(v)
+                print("    %-50s\t%14.2f" %(k, float(v)))
+            print("")
+        print("= Totals =")
+        for k, v in totals.items():
+            print("%-54s\t%14.2f" %(k, v))
+
 # pass return value from get_costs()
-def print_results(costs, use_json=False, use_csv=False, start=None, end=None):
+def print_cost_results(costs, use_json=False, use_csv=False, start=None, end=None):
+    if len(costs) == 0:
+        return
+
     if use_json == True:
         print(json.dumps(costs, sort_keys=True, indent=4))
     elif use_csv == True:
@@ -257,11 +418,10 @@ def print_results(costs, use_json=False, use_csv=False, start=None, end=None):
         for cost in flat_costs:
             csv_writer.writerow(cost)
     else:
-        out = consolidate_by_group(costs)
+        out = consolidate_costs_by_group(costs)
         print("\nSummary of costs: %s - %s\n" %(start, end))
         # print header.  hard-coded for now
-        print("%s %61s" %("Group", "Cost"))
-        print("%s %61s" %("-----", "----"))
+        print("%s %61s" %("= Group =", "= Cost ="))
         for cost in out:
             print("%-54s\t%14.2f %s"          \
                   %(cost['group'],
@@ -270,27 +430,46 @@ def print_results(costs, use_json=False, use_csv=False, start=None, end=None):
 
 # human-readable option currently not used, so hide it from usage
 def print_usage():
-     print("costreporter.py <options>\n"
-           "\tOptions are:\n\n"
-           "\t--help - Display this help message\n"
-           "\t-p --profile <profile name> - AWS profile name (can be used instead of -a and -s options)\n"
-           "\t-a --accesskey <access key> - AWS access key\n"
-           "\t-s --secretkey <secret key> - AWS secret key\n"
-           #"\t-r --regions <region1,region2,...> - A list of AWS regions.  If this option is omitted, all regions will be checked.\n"
-           "\t-t --timerange - Time range as <start,end> time in format <YYYY-MM-DD>,<YYYY-MM-DD>\n"
-           #"\t-h --human-readable <'k', 'm', or 'g'> display results in KB, MB, or GB.\n"
-           "\t-j --json - Output in JSON format.\n"
-           "\t-c --csv - Output as CSV.  Not compatible with --json.\n"
-           "\t-d --dimension <dimension> - Group output by dimension (examples: AZ,INSTANCE_TYPE,LINKED_ACCOUNT,OPERATION,PURCHASE_TYPE,REGION,SERVICE,USAGE_TYPE,USAGE_TYPE_GROUP,RECORD_TYPE,OPERATING_SYSTEM,TENANCY,SCOPE,PLATFORM,SUBSCRIPTION_ID,LEGAL_ENTITY_NAME,DEPLOYMENT_OPTION,DATABASE_ENGINE,CACHE_ENGINE,INSTANCE_TYPE_FAMILY)\n"
-           "\t-g --tag <tag name> - Group by tag name (list of names in format Tag1,Tag2,...,TagN).\n"
-           "\t-i --interval <interval> - Dumps stats at <interval> granularity.  Valid values are MONTHLY (default) and DAILY."
-           #"\t-b --abbrv - Output service abbreviations.\n\n"
-           "\tOne of the following three parameters are required:\n"
-           "\t\t1. Both the -a and -s options.\n"
-           "\t\t2. The -p option.\n"
-           "\t\t3. A valid " + FC_AWS_ENV + " environment variable.\n\n"
-           "\tDepending on the number of EBS volumes being analyzed, this tool may take several minutes to run.")
+     print("costreporter.py <command> [options]\n")
 
+     print("    Command must be one of the following:\n")
+     for cmd, desc in FC_COMMANDS.items():
+         print("    %s - %s" %(cmd, desc))
+     print("\n    General options are:\n\n"
+           "        -h --help - Display this help message\n"
+           "        -p --profile <profile name> - AWS profile name\n"
+           "                (can be used instead of -a and -s options)\n"
+           "        -a --accesskey <access key> - AWS access key\n"
+           "        -s --secretkey <secret key> - AWS secret key\n"
+           #"       -r --regions <region1,region2,...> - A list of AWS regions.  If this option is omitted, all regions will be checked.\n" # currently not in use
+           "        -j --json - Output in JSON format.\n"
+           "        -c --csv - Output as CSV.  Not compatible with --json\n"
+           "                (currently not available for 'recommend' command).\n")
+     print("    Options for 'cost' and 'coverage' commands:\n\n"
+           "        -t --timerange - Time range as <start,end> time\n"
+           "                in format <YYYY-MM-DD>,<YYYY-MM-DD> (required)\n"
+           "        -d --dimension <dimension> - Group output by dimension\n"
+           "                (examples: AZ,INSTANCE_TYPE,LINKED_ACCOUNT,OPERATION,\n"
+           "                 PURCHASE_TYPE,REGION,SERVICE (default),USAGE_TYPE,\n"
+           "                 USAGE_TYPE_GROUP,RECORD_TYPE,OPERATING_SYSTEM,\n"
+           "                 TENANCY,SCOPE,PLATFORM,SUBSCRIPTION_ID,LEGAL_ENTITY_NAME,\n"
+           "                 DEPLOYMENT_OPTION,DATABASE_ENGINE,CACHE_ENGINE,\n"
+           "                 INSTANCE_TYPE_FAMILY)\n"
+           "        -g --tag <tag name> - Group by tag name\n"
+           "                (list of names in format Tag1,Tag2,...,TagN).\n"
+           "        -i --interval <interval> - Dumps stats at <interval> granularity.\n"
+           "                Valid values are MONTHLY (default) and DAILY.\n")
+     print("    Options for 'recommend' command:\n\n"
+           "        -l --lookback <lookback> - Lookback period for recommendations.\n"
+           "                Valid values are SEVEN_DAYS, THIRTY_DAYS,\n"
+           "                SIXTY_DAYS (default)\n"
+           "        -r --service <service> - Service for recommendations.\n"
+           "                Valid values are EC2 (default) and RDS\n")
+           #"    -b --abbrv - Output service abbreviations.\n\n"
+     print("    One of the following three parameters are required:\n"
+           "        1. Both the -a and -s options.\n"
+           "        2. The -p option.\n"
+           "        3. A valid " + FC_AWS_ENV + " environment variable.")
 
 def parse_options(argv):
     parser = argparse.ArgumentParser(prog="costreporter.py",
@@ -299,38 +478,45 @@ def parse_options(argv):
     parser.add_argument("-p", "--profile", type=str, required=False)
     parser.add_argument("-a", "--access-key", type=str, required=False)
     parser.add_argument("-s", "--secret-key", type=str, required=False)
-    parser.add_argument("-r", "--regions", type=str, default="")
-    parser.add_argument("-t", "--timerange", type=str, default="")
-    #parser.add_argument("-h", "--human_readable", type=str, required=False, default='')
+    parser.add_argument("-z", "--regions", type=str, default="") #dummy
+    parser.add_argument("-t", "--timerange", type=str, default="dummy,dummy")
     parser.add_argument("-j", "--json", action="store_true", default=False)
     parser.add_argument("-c", "--csv", action="store_true", default=False)
     parser.add_argument("-d", "--dimension", type=str, default="")
     parser.add_argument("-g", "--tag", type=str, default="")
     parser.add_argument("-i", "--interval", type=str, default="MONTHLY")
-
+    parser.add_argument("-l", "--lookback", type=str, default="SIXTY_DAYS")
+    parser.add_argument("-r", "--service", type=str, default="EC2")
 
     args = parser.parse_args(argv)
     if (len(args.regions) == 0):
-        return args.profile, args.access_key, args.secret_key, [], args.timerange, args.json, args.csv, args.dimension, args.tag, args.interval
+        return args.profile, args.access_key, args.secret_key, [], args.timerange, args.json, args.csv, args.dimension, args.tag, args.interval, args.lookback, args.service
     else:
-        return args.profile, args.access_key, args.secret_key, args.regions.split(','), args.timerange, args.json, args.csv, args.dimension, args.tag, args.interval
+        return args.profile, args.access_key, args.secret_key, args.regions.split(','), args.timerange, args.json, args.csv, args.dimension, args.tag, args.interval, args.lookback, args.service
 
 
 def parse_args(argv):
     # ArgumentParser's built-in way of automatically handling -h and --help
     # leaves much to be desired, so using this hack instead.
     for arg in argv:
-        if (arg == '--help'):
+        if arg == '--help' or arg == '-h':
             print_usage()
             os._exit(0)
 
-    p, a, s, rList, t, j, c, d, g, i = parse_options(argv[1:])
+    cmd = argv[1]
 
-    return p, a, s, rList, t, j, c, d, g, i
+    p, a, s, rList, t, j, c, d, g, i, l, r = parse_options(argv[2:])
+
+    return cmd, p, a, s, rList, t, j, c, d, g, i, l, r
 
 
 if __name__ == "__main__":
-    p, a, s, rList, t, j, c, d, g, i = parse_args(sys.argv)
+    cmd, p, a, s, rList, t, j, c, d, g, i, l, r = parse_args(sys.argv)
+
+    if cmd not in FC_COMMANDS.keys():
+        print_usage()
+        print("\nError: invalid command %s" %cmd)
+        os._exit(1)
 
     # need either -a and -s, -p, or AWS_DEFAULT_PROFILE environment variable
     if not a and not s and not p:
@@ -365,8 +551,12 @@ if __name__ == "__main__":
                     os._exit(1)
 
             # get access/secret keys
-            a = pFile.readline().strip().split(" ")[2]
-            s = pFile.readline().strip().split(" ")[2]
+            for _dummy in range(0, 2):
+                line = pFile.readline()
+                if "aws_secret_access_key" in line:
+                    s = line.strip().split(" ")[2]
+                elif "aws_access_key_id" in line:
+                    a = line.strip().split(" ")[2]
 
         except:
             print("Error: reading credentials for profile %s." %p)
@@ -374,9 +564,9 @@ if __name__ == "__main__":
 
     #if (len(rList) == 0):
     #    rList = AWS_REGIONS
-    # values are not divided by region, instead use -d REGION flag
-    # so just use arbitrary region
-    rList = ["us-east-1"]
+    # values are not divided by region, users should instead use -d REGION
+    # option if they want to group results by region
+    rList = ["us-east-1"] # just use arbitrary region
 
     if j == True and c == True:
         print("Error: cannot specify both -j and -c")
@@ -385,15 +575,17 @@ if __name__ == "__main__":
     time = t.split(",")
 
     # simple sanity check #1
-    if len(time) != 2:
+    if len(time) != 2 and cmd != "recommend":
         print("Error: proper timerange format for <start,end> times is <YYYY-MM-DD>,<YYYY-MM-DD>")
         os._exit(1)
+
     start_time = time[0]
     end_time = time[1]
 
     # simple sanity check #2
-    if re.match(FC_MATCH_DATE, start_time) == None or \
-       re.match(FC_MATCH_DATE, end_time) == None:
+    if cmd != "recommend" and                          \
+       (re.match(FC_MATCH_DATE, start_time) == None or \
+       re.match(FC_MATCH_DATE, end_time) == None):
         print("start_time = %s, match = %s" %(start_time, re.match(FC_MATCH_DATE, start_time)))
         print("end_time = %s, match = %s" %(end_time, re.match(FC_MATCH_DATE, end_time)))
         print("Error: proper timerange format for start, end times is <YYYY-MM-DD>,<YYYY-MM-DD>")
@@ -416,8 +608,16 @@ if __name__ == "__main__":
         # comment out customer service abbreviations for now
         #abbrv = build_abbreviations(a, s, rList[0], start_time, end_time)
         #ABBRV.update(abbrv)
-        costs = get_costs(a, s, rList, start_time, end_time, d, g, i)
-        print_results(costs, j, c, start_time, end_time)
+
+        if cmd == "cost":
+            costs = get_costs(a, s, rList, start_time, end_time, d, g, i)
+            print_cost_results(costs, j, c, start_time, end_time)
+        elif cmd == "recommend":
+            recs = get_reserve_instance_recs(a, s, r, l)
+            print_ri_recs_results(recs)
+        elif cmd == "coverage":
+            covs = get_reservation_coverage(a, s, rList, start_time, end_time, d, g, i)
+            print_coverage_results(covs, j, c, start_time, end_time)
     except:
         e = sys.exc_info()
         traceback.print_exc()
